d69c1f1d0a23ddd80b108527dad8f46277301a1f
kakecat <125948218+kakecat>
2023-04-16 06:39:10 +0000

    モデルのアソシエーションとenum追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+
+  has_many :order_detils
+  belongs_to :customer
+
+  enum payment_method: {credit_card:0, transfer:1}
+  enum status: {waiting_for_payment:0, payment_confirmation:1, in_production:2, shipping_preparation:3, shipped:4}
 end

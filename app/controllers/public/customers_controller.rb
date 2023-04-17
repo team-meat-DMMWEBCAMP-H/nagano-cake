0f5935897bccf38561e9120d60f78bea51fc9a7b
kakecat <125948218+kakecat>
2023-04-17 11:24:48 +0000

    退会機能完了！

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,13 +4,19 @@ class Public::CustomersController < ApplicationController
 
   def edit
   end
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to public_customer_mypage_path
+  end
 
   def check
     @customer = current_customer
-    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    # is_activeカラムをtrueに変更することにより削除フラグを立てる
     @customer.update(is_active: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
+
 end

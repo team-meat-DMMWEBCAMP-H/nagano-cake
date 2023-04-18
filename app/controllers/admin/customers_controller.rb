e19d8245c554213e9929dd3a89c062f0fb7e05c0
kakecat <125948218+kakecat>
2023-04-18 05:12:09 +0000

    コード追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,15 +6,5 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
-  end
-  
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to admin_customer_path
-    else
-      render "edit"
-    end
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,9 @@
 class Admin::OrdersController < ApplicationController
+  def index
+    @orders = Order.page(params[:page]).per(10)
+    @order_details = OrderDetail.all
+  end
+
   def show
   end
 end

6457218b705391361f56f801895e7ea9035fb074
kakecat <125948218+kakecat>
2023-04-20 07:23:00 +0000

    追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: "homes#top"
-    resources :orders, only: [:index, :show, :update,]
+    resources :orders, only: [:show, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :edit, :update, :create]
     resources :items, only: [:index, :new, :show, :edit, :update, :create]

d7cdb8aaeb817472f7e68997ca1457fad1164571
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-04-20 07:23:35 +0000

    taa

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,14 @@
 class Public::CartItemsController < ApplicationController
+    # before_action :authenticate_customer!
+    before_action :set_cart_item, only: [:increase, :decrease, :destroy]
+
   def index
+    @cart_items = current_customer.cart_items.all
   end
+  
+  
+  def create
+    @cart_item = current_
+  end  
+  
 end

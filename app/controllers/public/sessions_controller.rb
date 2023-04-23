9217969319067b28f5014707a9c9b12da850e5e2
Yuuta1467 <s1341011we@gmail.com>
2023-04-23 08:23:59 +0000

    なぜ？

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+
+  #before action :reject_customer, only: [create]
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +21,15 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
+        redirect_to new_customer_registration
+      end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params

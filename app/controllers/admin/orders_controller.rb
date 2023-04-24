235a7bf361d8a3cc8d9041ed81190bf604722d8e
kakecat <125948218+kakecat>
2023-04-24 05:25:01 +0000

    エラー解決中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,12 +12,16 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to request.referer
+  @order = Order.find(params[:id])
+  @order_details = OrderDetail.where(order_id: params[:id])
+  if @order.update(order_params)
+    @order_details.update_all(production_status: 1) if @order.status == "payment_confirmation"
+    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+  end
+  redirect_to admin_order_path(@order)
   end
 
-private
+  private
   def order_params
     params.require(:order).permit(:status)
   end
